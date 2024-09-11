1c0322d453555513dcf2f63ba624395414714a3a
Mikhail Ilin <mikhail.ilin93@gmail.com>
2024-09-11 20:57:57 +0500

    PrintCurrentDateMain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import application.salary
 import application.db.people as people
+import datetime
+
+current_date = datetime.date.today().isoformat()
+print(current_date)
 
 if __name__ == '__main__':
         application.salary.calculate_salary()
